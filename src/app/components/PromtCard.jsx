4eb044f9a1e44fc0ede439020ebeb78a15e2c6e3
pavan <67680131+pavan2580-dmpt@users.noreply.github.com>
2023-10-20 23:41:42 +0530

    Update PromtCard.jsx

--- src/app/components/PromtCard.jsx
+++ src/app/components/PromtCard.jsx
@@ -47,10 +47,7 @@ async function handleDelete(){
 
   return (
     <>
-      {
-        !post:<div>Loading</div> :(
-        <>
-          <div className="w-full bg-white rounded-lg h-[fit] border-2 border-solid border-gray-400 p-2 box-border md:w-[400px] md:justify-start shadow-xl ">
+      <div className="w-full bg-white rounded-lg h-[fit] border-2 border-solid border-gray-400 p-2 box-border md:w-[400px] md:justify-start shadow-xl ">
         <div className="flex w-full h-fit items-center justify-between pr-3">
               <div className="flex gap-3  ">
               <span >
@@ -107,9 +104,6 @@ async function handleDelete(){
               }
        
       </div>
-        </>
-        )
-      }
       
     </>
   );
